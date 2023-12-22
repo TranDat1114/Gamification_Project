8c4bc6a0dc5c0603c2d8322471dde968f13072ee
trandat1114 <cctalk1114@gmail.com>
2023-12-22 09:34:14 +0700

    Update app.min.css and ImportFile.razor

---
+++ FPT_Vote/Models/GroupData.cs
@@ -0,0 +1,8 @@
+﻿namespace FPT_Vote;
+
+public class GroupData
+{
+    public int GroupId { get; set; }=0;
+    public string GroupName { get; set; } = string.Empty;
+    public int Point { get; set; } = 0;
+}
